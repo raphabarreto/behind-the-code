7301a4efd14420508106c66426fbe43a87ec488c
Raphael Barreto <tnt.raphael@gmail.com>
2019-11-05 16:30:22 -0300

    Adicionado forgot and reset password

--- backend/app/Controllers/Http/ForgotPasswordController.js
+++ backend/app/Controllers/Http/ForgotPasswordController.js
@@ -1,6 +1,10 @@
 'use strict'
 
-const Mail = use('Mail')
+const { randomBytes } = require('crypto');
+const { promisify } = require('util');
+
+const Mail = use('Mail');
+const Env = use('Env')
 
 /** @type {typeof import ('@adonisjs/lucid/src/Lucid/Model')} */
 const User = use('App/Models/User');
@@ -11,7 +15,20 @@ class ForgotPasswordController {
 
       const user = await User.findByOrFail('email', email);
 
-      await Mail.send('emails.forgotpassword', { name: user.name }, (message) => {
+      const random = await promisify(randomBytes)(16);
+      const token = random.toString('hex');
+
+      await user.tokens().create({
+        token,
+        type: 'forgotpassword',
+      })
+
+      const resetPasswordUrl = `${Env.get('FRONT_URL')}/reset?token=${token}`; 
+
+      await Mail.send(
+        'emails.forgotpassword',
+        { name: user.name, token, resetPasswordUrl }, 
+        (message) => {
         message
           .to(user.email)
           .from('tnt.raphael@gmail.com')
